6559e16676427d3b1d1ac82dc2b213575ee17835
Vivek Singh <rajputvivek649@gmail.com>
2024-12-18 12:35:16 +0530

    dahboard add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
             <div className="w-full md:w-1/2 lg:w-1/3 flex items-center">
               <form className="space-y-4 px-7 py-10 rounded-lg w-full bg-white">
                 <div className="logo flex items-center justify-center">
-                  <img src="/logo.svg" alt="" />
+                  <img src="/assets/logo.svg" alt="" />
                 </div>
                 <div>
                   <label
@@ -46,6 +46,27 @@ function Home() {
                   className="w-full bg-primary-600 text-white py-3 rounded-lg hover:bg-primary-700 transition duration-300"
                 >
                   Login
+                </button>
+                <button
+                  type="button"
+                  class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                >
+                  <a href="/admin/dashboard">View Dashboard</a>
+                  <svg
+                    class="shrink-0 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="m12 5 7 7-7 7"></path>
+                  </svg>
                 </button>
               </form>
             </div>
